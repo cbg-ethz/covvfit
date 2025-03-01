0f7c0426eab3f1d0f226ad9ca6ee968833a73ae4
Paweł Czyż <pczyz@protonmail.com>
2025-03-01 18:06:42 +0100

    Add one JIT

--- src/covvfit/_quasimultinomial.py
+++ src/covvfit/_quasimultinomial.py
@@ -285,18 +285,16 @@ def get_confidence_bands_logit(
     ]
 
     logit_se = []
-    for i, ts in enumerate(ts):
-        # Compute the Jacobian of the transformation and project standard errors
-        def _aux(t):
-            jacobian = jax.jacobian(_create_logit_predictions_fn(n_variants, i, t))(
-                theta
-            )
-            standard_errors = get_standard_errors(
-                jacobian=jacobian, covariance=covariance
-            )
-            return standard_errors
 
-        se = jax.vmap(_aux)(ts)
+    # Compute the Jacobian of the transformation and project standard errors
+    @jax.jit
+    def _aux(i: int, t):
+        jacobian = jax.jacobian(_create_logit_predictions_fn(n_variants, i, t))(theta)
+        standard_errors = get_standard_errors(jacobian=jacobian, covariance=covariance)
+        return standard_errors
+
+    for i, ts in enumerate(ts):
+        se = jax.vmap(lambda t: _aux(i, t))(ts)
         logit_se.append(se)
 
     # Compute confidence intervals on the logit scale
