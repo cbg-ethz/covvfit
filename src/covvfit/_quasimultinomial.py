721fdeb12d54ba83d042b1ec210d735030d5476c
Paweł Czyż <pczyz@protonmail.com>
2025-03-01 18:47:02 +0100

    Make logit confidence bands faster (#58)
    * Make logit confidence bands faster
    
    * Remove previous implementation
    
    * Compute Jacobians sequentially
    
    * Add one JIT

--- src/covvfit/_quasimultinomial.py
+++ src/covvfit/_quasimultinomial.py
@@ -285,11 +285,17 @@ def get_confidence_bands_logit(
     ]
 
     logit_se = []
-    for i, ts in enumerate(ts):
-        # Compute the Jacobian of the transformation and project standard errors
-        jacobian = jax.jacobian(_create_logit_predictions_fn(n_variants, i, ts))(theta)
+
+    # Compute the Jacobian of the transformation and project standard errors
+    @jax.jit
+    def _aux(i: int, t):
+        jacobian = jax.jacobian(_create_logit_predictions_fn(n_variants, i, t))(theta)
         standard_errors = get_standard_errors(jacobian=jacobian, covariance=covariance)
-        logit_se.append(standard_errors)
+        return standard_errors
+
+    for i, ts in enumerate(ts):
+        se = jax.vmap(lambda t: _aux(i, t))(ts)
+        logit_se.append(se)
 
     # Compute confidence intervals on the logit scale
     logit_confint = [
